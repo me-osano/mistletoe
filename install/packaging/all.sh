4ffa92fab04eb7c1554795d638c08bf3b52cbead
Enosh Misonge Osano <osanoenosh@gmail.com>
2026-02-12 01:14:07 +0300

    Edited mise

--- install/packaging/all.sh
+++ install/packaging/all.sh
@@ -1,5 +1,5 @@
 run_logged $MISTLETOE_INSTALL/packaging/base.sh
 run_logged $MISTLETOE_INSTALL/packaging/icons.sh
-run_logged $MISTLETOE_INSTALL/packaging/nvim.sh
+#run_logged $MISTLETOE_INSTALL/packaging/nvim.sh
 run_logged $MISTLETOE_INSTALL/packaging/webapps.sh
 run_logged $MISTLETOE_INSTALL/packaging/tuis.sh
